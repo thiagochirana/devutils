588cb8633a64e588d71fc9f964319a5025278d6c
Curumin <engsw.thiago@gmail.com>
2025-04-10 01:04:26 -0300

    feat: add tailwind and a Hello World!

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="bg-indigo-500 font-extrabold text-9xl text-white">HELLO WORLD!</h1>
 </template>
